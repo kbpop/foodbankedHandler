b3be3102546673bf7ce7ccacf42e6e451b0f28b7
kbpop <kp.petzold.bradley@gmail.com>
2026-07-04 17:00:46 -0400

    more intermiddent stuff

--- app/db_utils.py
+++ app/db_utils.py
@@ -10,7 +10,7 @@ def connect_to_db():
     try:
         # Establish the connection
         connection = mysql.connector.connect(
-            host="mysql_container",
+            host="172.17.0.2",
             database=os.getenv("MYSQL_DATABASE"),
             user=os.getenv("MYSQL_USER"),
             password=os.getenv("MYSQL_PASSWORD")
